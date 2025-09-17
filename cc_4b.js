2fb23217ea5d3c5b266d278ae7956fe0276f3b69
Joshua Glaser <jdglaser@usf.edu>
2025-09-17 17:36:15 -0400

    first commit

---
+++ cc_4b.js
@@ -0,0 +1,50 @@
+
+// Step 2 building the array
+let products = [{
+    sku: "sku-1001",
+    name: "Hammer",
+    category: "Tools",
+    price: 19.99,
+    inventory: 25,
+},
+{
+    sku: "sku-1002",
+    name: "Bread",
+    category: "Grocery",
+    price: 2.99,
+    inventory: 120,
+},
+{
+    sku: "sku-1003",
+    name: "Computer",
+    category: "Electronics",
+    price: 699.99,
+    inventory: 15,
+},
+{
+    sku: "sku-1004",
+    name: "Hose",
+    category: "Lawn and Garden",
+    price: 29.99,
+    inventory: 20,
+},
+{
+    sku: "sku-1005",
+    name: "Shirt",
+    category: "Clothes",
+    price: 12.99,
+    inventory: 10,
+},
+]
+// Step 3
+let discounts
+switch(products.category){
+
+    case "electronics":
+    discount = .2
+    break
+
+    default
+    break
+        
+}
